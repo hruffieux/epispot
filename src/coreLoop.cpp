95cb1c26b12c04ec637851bce2d74b86c5cfb107
HélènRuffieux <helene.ruffieux@mrc-bsu.cam.ac.uk>
2020-07-05 19:30:13 +0200

    Cleaned unused C++ code.

--- src/coreLoop.cpp
+++ src/coreLoop.cpp
@@ -3,8 +3,7 @@
  * This file is part of the `epispot` R package:
  *     https://github.com/hruffieux/epispot
  *
- * Functions for computationally expensive updates in algorithms without
- * external information.
+ * Functions for computationally expensive updates.
  *
  * These functions use Eigen::Map to pass large matrices by reference from R.
  * Given dimensionalities involved in some applications, copying such matrices
@@ -15,64 +14,22 @@
 #include "utils.h"
 
 
-// for epispot_core function
+// for epispot_dual_core function
 // [[Rcpp::export]]
-void coreLoop(const MapMat X,
-              const MapMat Y,
-              MapArr2D gam_vb,
-              const MapArr1D log_om,
-              const MapArr1D log_1_min_om,
-              const double log_sig2_inv_vb,
-              const MapArr1D log_tau_vb,
-              MapMat m1_beta,
-              MapMat mat_x_m1,
-              MapArr2D mu_beta_vb,
-              const MapArr1D sig2_beta_vb,
-              const MapArr1D tau_vb,
-              const MapArr1D shuffled_ind,
-              const double c = 1) {
-
-  const Arr1D cst = - (log_tau_vb + log_sig2_inv_vb + log(sig2_beta_vb)) / 2;
-
-  for (int i = 0; i < X.cols(); ++i) {
-
-    int j = shuffled_ind[i];
-
-    mat_x_m1.noalias() -= X.col(j) * m1_beta.row(j);
-
-    mu_beta_vb.row(j) = c * sig2_beta_vb * tau_vb *
-      ((Y - mat_x_m1).transpose() * X.col(j)).array();
-
-    gam_vb.row(j) = exp(-logOnePlusExp(c * (log_1_min_om(j) - log_om(j) -
-      mu_beta_vb.row(j).transpose().square() / (2 * sig2_beta_vb) + cst)));
-
-    m1_beta.row(j) = mu_beta_vb.row(j) * gam_vb.row(j);
-
-    mat_x_m1.noalias() += X.col(j) * m1_beta.row(j);
-
-  }
-
-}
-
-
-
-// for epispot_z_core and epispot_mix_core function
-// [[Rcpp::export]]
-void coreZLoop(const MapMat X,
-               const MapMat Y,
-               MapArr2D gam_vb,
-               const MapArr1D log_om,
-               const MapArr1D log_1_min_om,
-               const double log_sig2_inv_vb,
-               const MapArr1D log_tau_vb,
-               MapMat m1_beta,
-               MapMat mat_x_m1,
-               MapMat mat_z_mu,
-               MapArr2D mu_beta_vb,
-               const MapArr1D sig2_beta_vb,
-               const MapArr1D tau_vb,
-               const MapArr1D shuffled_ind,
-               const double c = 1) {
+void coreDualLoop(const MapMat X,
+                  const MapMat Y,
+                  MapArr2D gam_vb,
+                  const MapArr2D log_Phi_mu_theta_plus_rho,
+                  const MapArr2D log_1_min_Phi_mu_theta_plus_rho,
+                  const double log_sig2_inv_vb,
+                  const MapArr1D log_tau_vb,
+                  MapMat m1_beta,
+                  MapMat mat_x_m1,
+                  MapArr2D mu_beta_vb,
+                  const MapArr1D sig2_beta_vb,
+                  const MapArr1D tau_vb,
+                  const MapArr1D shuffled_ind,
+                  const double c = 1) {
 
   const Arr1D cst = -(log_tau_vb + log_sig2_inv_vb + log(sig2_beta_vb) )/ 2;
 
@@ -83,85 +40,11 @@ void coreZLoop(const MapMat X,
     mat_x_m1.noalias() -= X.col(j) * m1_beta.row(j);
 
     mu_beta_vb.row(j) = c * sig2_beta_vb * tau_vb *
-      ((Y - mat_x_m1 - mat_z_mu).transpose() * X.col(j)).array();
+      ((Y - mat_x_m1).transpose() * X.col(j)).array();
 
-    gam_vb.row(j) = exp(-logOnePlusExp(c * (log_1_min_om(j) - log_om(j) -
-      mu_beta_vb.row(j).transpose().square() / (2 * sig2_beta_vb) + cst)));
-
-    m1_beta.row(j) = mu_beta_vb.row(j) * gam_vb.row(j);
-
-    mat_x_m1.noalias() += X.col(j) * m1_beta.row(j);
-
-  }
-
-}
-
-
-// for epispot_logit_core function
-// [[Rcpp::export]]
-void coreLogitLoop(const MapMat X,
-                   const MapArr2D Y,
-                   MapArr2D gam_vb,
-                   const MapArr1D log_om,
-                   const MapArr1D log_1_min_om,
-                   const double log_sig2_inv_vb,
-                   MapMat m1_beta,
-                   MapArr2D mat_x_m1,
-                   MapArr2D mat_z_mu,
-                   MapArr2D mu_beta_vb,
-                   const MapArr2D psi_vb,
-                   const MapArr2D sig2_beta_vb,
-                   const MapArr1D shuffled_ind) {
-
-  for (int i = 0; i < X.cols(); ++i) {
-
-    int j = shuffled_ind[i];
-
-    mat_x_m1.matrix().noalias() -= X.col(j) * m1_beta.row(j);
-
-    mu_beta_vb.row(j) = sig2_beta_vb.row(j) * (X.col(j).transpose() * (Y - 2 * psi_vb * (mat_x_m1 + mat_z_mu)).matrix()).array();
-
-    gam_vb.row(j) = exp(-logOnePlusExp(log_1_min_om(j) - log_om(j) -
-      log_sig2_inv_vb / 2 - mu_beta_vb.row(j).square() / (2 * sig2_beta_vb.row(j)) -
-      log(sig2_beta_vb.row(j)) / 2));
-
-    m1_beta.row(j) = mu_beta_vb.row(j) * gam_vb.row(j);
-
-    mat_x_m1.matrix().noalias() += X.col(j) * m1_beta.row(j);
-
-  }
-
-}
-
-
-
-// for epispot_probit_core function
-// [[Rcpp::export]]
-void coreProbitLoop(const MapMat X,
-                    const MapMat W,
-                    MapArr2D gam_vb,
-                    const MapArr1D log_om,
-                    const MapArr1D log_1_min_om,
-                    const double log_sig2_inv_vb,
-                    MapMat m1_beta,
-                    MapMat mat_x_m1,
-                    MapMat mat_z_mu,
-                    MapArr2D mu_beta_vb,
-                    const double sig2_beta_vb,
-                    const MapArr1D shuffled_ind) {
-
-  const double cst = -(log_sig2_inv_vb + log(sig2_beta_vb) )/ 2;
-
-  for (int i = 0; i < X.cols(); ++i) {
-
-    int j = shuffled_ind[i];
-
-    mat_x_m1.noalias() -= X.col(j) * m1_beta.row(j);
-
-    mu_beta_vb.row(j) = sig2_beta_vb * ((W - mat_x_m1 - mat_z_mu).transpose() * X.col(j)).array();
-
-    gam_vb.row(j) = exp(-logOnePlusExp(log_1_min_om(j) - log_om(j) -
-      mu_beta_vb.row(j).square() / (2 * sig2_beta_vb) + cst));
+    gam_vb.row(j) = exp(-logOnePlusExp(c * (log_1_min_Phi_mu_theta_plus_rho.row(j) -
+      log_Phi_mu_theta_plus_rho.row(j) - mu_beta_vb.row(j).square() / (2 * sig2_beta_vb.transpose()) +
+      cst.transpose())));
 
     m1_beta.row(j) = mu_beta_vb.row(j) * gam_vb.row(j);
 
@@ -171,3 +54,38 @@ void coreProbitLoop(const MapMat X,
 
 }
 
+
+// [[Rcpp::export]]
+void coreDualInfoLoop(const MapMat V,
+                  const MapMat W,
+                  MapArr1D zeta_vb,
+                  const MapArr1D log_om,
+                  const MapArr1D log_1_min_om,
+                  const double s2,
+                  MapVec m1_xi,
+                  MapMat mat_v_mu,
+                  MapArr1D mu_xi_vb,
+                  const double sig2_xi_vb,
+                  const MapArr1D shuffled_ind,
+                  const double c = 1) {
+
+  const double cst = (log(s2) - log(sig2_xi_vb))/ 2;
+
+  for (int i = 0; i < V.cols(); ++i) {
+
+    int j = shuffled_ind[i];
+
+    mat_v_mu.colwise() -=  V.col(j) * m1_xi(j);
+
+    mu_xi_vb(j) = c * sig2_xi_vb * ((W - mat_v_mu).transpose() * V.col(j)).sum();
+
+    zeta_vb(j) = 1 / (1 + exp(c * (log_1_min_om(j) - log_om(j) -
+      mu_xi_vb(j) * mu_xi_vb(j) / (2 * sig2_xi_vb) + cst)));
+
+    m1_xi(j) = mu_xi_vb(j) * zeta_vb(j);
+
+    mat_v_mu.colwise() +=  V.col(j) * m1_xi(j);
+
+  }
+
+}
